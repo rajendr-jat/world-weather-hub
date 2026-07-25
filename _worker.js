ddcba97c905feb3b81435077a12edbfeabf6be01
rajendr-jat <rajendrajat08111@gmail.com>
2026-07-25 17:03:46 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith('/weather/')) {
-      url.pathname = '/index.html';
+      url.pathname = '/';
       return env.ASSETS.fetch(new Request(url, request));
     }
 
