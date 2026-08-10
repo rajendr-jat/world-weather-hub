590d5fa90a5cd3b1c3715e6bbc0e83a266eb4ea1
rajendr-jat <rajendrajat08111@gmail.com>
2026-08-10 09:10:15 +0530

    Fix ensureActiveCity function definition

--- api.js
+++ api.js
@@ -62,7 +62,7 @@ async function resolveCurrentCity() {
 let ACTIVE_CITY = null;
 let _activeCityPromise = null;
 
-function ensureActiveCity() {
+
 function ensureActiveCity() {
     if (!_activeCityPromise) {
         _activeCityPromise = resolveCurrentCity().then(city => {
