0cdf43d0f7f0aab187ee8cac4c944cb2a26511cc
rajendr-jat <rajendrajat08111@gmail.com>
2026-08-11 10:49:20 +0530

    Update sitemap-static.xml.js

--- functions/sitemap-static.xml.js
+++ functions/sitemap-static.xml.js
@@ -1,29 +1,20 @@
 const SITE = "https://world-weather-hub.pages.dev";
 
-const STATIC_PAGES = [
-  { path: "/", priority: "1.0", changefreq: "hourly" },
-  { path: "/forecast.html", priority: "0.9", changefreq: "hourly" },
-  { path: "/aqi.html", priority: "0.9", changefreq: "hourly" },
-  { path: "/history.html", priority: "0.7", changefreq: "daily" }
-];
-
 export async function onRequest(context) {
-  const urls = STATIC_PAGES.map(p => `
-  <url>
-    <loc>${SITE}${p.path}</loc>
-    <changefreq>${p.changefreq}</changefreq>
-    <priority>${p.priority}</priority>
-  </url>`).join("");
-
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${SITE}/sitemap-static.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>${SITE}/sitemap-cities/1.xml</loc>
+  </sitemap>
+</sitemapindex>`;
 
   return new Response(xml, {
     headers: {
       "Content-Type": "application/xml; charset=UTF-8",
-      "Cache-Control": "public, max-age=3600"
+      "Cache-Control": "no-cache"
     }
   });
 }
